0b47f6e8a071ad8171ac61b83ab71d36fae7a05c
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-07-20 19:01:49 +0200

    feat: add homepage cache header

--- hydra-login-consent/routes/index.go
+++ hydra-login-consent/routes/index.go
@@ -7,5 +7,6 @@ import (
 )
 
 func (h *Handler) Index(c *gin.Context) {
+	c.Header("Cache-Control", "max-age=3600, s-maxage=86400, stale-while-revalidate=86400, stale-if-error=86400")
 	c.HTML(http.StatusOK, "index.html", gin.H{})
 }
